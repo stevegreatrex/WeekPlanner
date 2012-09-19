0f71c0838bb4bed5a51630a885fe848778659c1f
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-09-19 19:39:12 +0100

    Added a couple of model classes

---
+++ WeekPlanner/Models/PlannerSegment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeekPlanner.Models
+{
+	public class PlannerSegment
+	{
+		public PlannerSegment()
+		{
+			this.Restrictions = new List<Restriction>();
+		}
+
+		public Guid Id { get; set; }
+
+		public DateTime Start { get; set; }
+
+		public IList<Restriction> Restrictions { get; private set; }
+	}
+}
